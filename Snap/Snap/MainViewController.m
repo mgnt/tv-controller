82d2b79a510f6f128105760a3c4b4449916a3b97
Elliot Lee <github.public@intelliot.com>
2015-10-04 09:44:32 -0700

    Add Snap sample

---
+++ Snap/Snap/MainViewController.m
@@ -0,0 +1,45 @@
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+@property (nonatomic, weak) IBOutlet UIImageView *sImageView;
+@property (nonatomic, weak) IBOutlet UIImageView *nImageView;
+@property (nonatomic, weak) IBOutlet UIImageView *aImageView;
+@property (nonatomic, weak) IBOutlet UIImageView *pImageView;
+@property (nonatomic, weak) IBOutlet UIImageView *jokerImageView;
+
+@property (nonatomic, weak) IBOutlet UIButton *hostGameButton;
+@property (nonatomic, weak) IBOutlet UIButton *joinGameButton;
+@property (nonatomic, weak) IBOutlet UIButton *singlePlayerGameButton;
+@end
+
+@implementation MainViewController
+
+@synthesize sImageView = _sImageView;
+@synthesize nImageView = _nImageView;
+@synthesize aImageView = _aImageView;
+@synthesize pImageView = _pImageView;
+@synthesize jokerImageView = _jokerImageView;
+
+@synthesize hostGameButton = _hostGameButton;
+@synthesize joinGameButton = _joinGameButton;
+@synthesize singlePlayerGameButton = _singlePlayerGameButton;
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+	return UIInterfaceOrientationIsLandscape(interfaceOrientation);
+}
+
+- (IBAction)hostGameAction:(id)sender
+{
+}
+
+- (IBAction)joinGameAction:(id)sender
+{
+}
+
+- (IBAction)singlePlayerGameAction:(id)sender
+{
+}
+
+@end
