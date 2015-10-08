182b8432832587d60c3c883fe96069942bb0850c
Elliot Lee <github.public@intelliot.com>
2015-10-07 23:03:20 -0700

    WIP - Start implementing broadcasting

--- Snap/TVC Networking/TVCServer.swift
+++ Snap/TVC Networking/TVCServer.swift
@@ -1,10 +1,35 @@
 
 import Foundation
+import UIKit
 
-class TVCServer: NSObject, NSNetServiceDelegate {
+// The Bonjour service type consists of an IANA service name (see RFC 6335)
+// prefixed by an underscore (as per RFC 2782).
+//
+// <http://www.ietf.org/rfc/rfc6335.txt>
+//
+// <http://www.ietf.org/rfc/rfc2782.txt>
+//
+// See Section 5.1 of RFC 6335 for the specifics requirements.
+//
+// - MUST be at least 1 character and no more than 15 characters long
+//
+// To avoid conflicts, you must register your service type with IANA before
+// shipping.
+//
+// To help network administrators identify your service, you should choose a
+// service name that's reasonably human readable.
+
+let kSnapBonjourType = "_snap._tcp." // TODO: make this settable by app
+
+
+/// TVCServer has no UI and will provide a delegate protocol for client connection events
+class TVCServer: NSObject, NSNetServiceDelegate, NSStreamDelegate {
     
     var maxClients: Int
     var connectedClients: Array<String> // mutable
+    var server: NSNetService
+    var isServerStarted = false
+    
 //    var session: MCSession // ?
     
     //MCNearbyServiceAdvertiser
@@ -14,27 +39,37 @@ class TVCServer: NSObject, NSNetServiceDelegate {
         connectedClients = Array()
 //        session = MCSession(peer: MCPeerID(displayName: "Some Display Name")) // TODO
         
+        // Create our server. We only want the service to be registered on
+        // local networks so we pass in the "local." domain.
+        self.server = NSNetService(domain: "local.", type: kSnapBonjourType, name: UIDevice.currentDevice().name, port: 0)
+        self.server.includesPeerToPeer = true
+        
         super.init()
+        
+        self.server.delegate = self
         
 //        session.delegate = self // ?
     }
     
-//    /// Broadcast availability of the service named by sessionID.
-//    /// TODO
-//    func startAcceptingConnections(sessionID: String) {
-//        // TODO: customize name
-//        // TODO: use sessionID
-//        
-////        session = MCSession(peer: MCPeerID(displayName: "Some Name")) // server mode? available?
-////        session.delegate = self
-//        
-//        // TODO: start broadcasting
-//    }
-//    
-//    // MARK: - MCNearbyServiceAdvertiserDelegate
-//    
-//    // Incoming invitation request.  Call the invitationHandler block with YES
-//    // and a valid session to connect the inviting peer to the session.
+    /// Broadcast availability of the service named by sessionID.
+    func startAcceptingConnections(sessionID: String) {
+        
+        // Advertise our server (start broadcasting).
+        self.server.publishWithOptions(NSNetServiceOptions.ListenForConnections)
+        self.isServerStarted = true // TODO: what's this used for?
+        
+        // TODO: use sessionID (or remove the sessionID parameter)
+        // TODO: remove session
+        
+        // TODO:
+        // Set up for a new game, which presents a Bonjour browser that displays other
+        // available games.
+    }
+    
+    // MARK: - MCNearbyServiceAdvertiserDelegate
+    
+    // Incoming invitation request.  Call the invitationHandler block with YES
+    // and a valid session to connect the inviting peer to the session.
 //    func advertiser(advertiser: MCNearbyServiceAdvertiser, didReceiveInvitationFromPeer peerID: MCPeerID, withContext context: NSData?, invitationHandler: (Bool, MCSession) -> Void)
 //    {
 //        print("TVCServer: connection request from peer \(peerID)")
